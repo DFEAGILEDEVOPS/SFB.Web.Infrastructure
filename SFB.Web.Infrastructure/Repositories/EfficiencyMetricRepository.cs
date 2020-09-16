473ece50beaf1c0bdd2c4f08fc70e886a6ab2bfa
Serkan Sozer <eserkansozer@hotmail.com>
2020-09-16 17:10:55 +0100

    AB#43311

--- SFB.Web.Infrastructure/Repositories/EfficiencyMetricRepository.cs
+++ SFB.Web.Infrastructure/Repositories/EfficiencyMetricRepository.cs
@@ -3,11 +3,8 @@ using Microsoft.Azure.Cosmos.Fluent;
 using SFB.Web.ApplicationCore.DataAccess;
 using SFB.Web.ApplicationCore.Entities;
 using SFB.Web.Infrastructure.Logging;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SFB.Web.Infrastructure.Repositories
@@ -32,53 +29,17 @@ namespace SFB.Web.Infrastructure.Repositories
             _databaseId = databaseId;
         }
 
-        public async Task<EfficiencyMetricDataObject> GetEfficiencyMetricDataObjectByUrnAsync(int urn)
+        public async Task<EfficiencyMetricParentDataObject> GetEfficiencyMetricDataObjectByUrnAsync(int urn)
         {
-            var container = _client.GetContainer(_databaseId, "EMPoc");
+            var container = _client.GetContainer(_databaseId, "EmData");
 
-            var queryString = $"SELECT * FROM c WHERE c.urn=@URN";
+            var queryString = $"SELECT * FROM c WHERE c.Urn=@URN";
 
             var queryDefinition = new QueryDefinition(queryString)
                 .WithParameter($"@URN", urn);
 
-            var feedIterator = container.GetItemQueryIterator<EfficiencyMetricDataObject>(queryDefinition, null);
-            var result = (await feedIterator.ReadNextAsync()).FirstOrDefault();
-
-            return result;
-        }
-
-        public async Task<List<EfficiencyMetricDataObject>> GetMultipleSchoolDataObjectsByUrnsAsync(List<int> urns)
-        {
-            var container = _client.GetContainer(_databaseId, "EMPoc");
-
-            var sb = new StringBuilder();
-            urns.ForEach(u => sb.Append(u + ","));
-
-            var queryString = $"SELECT * FROM c WHERE c.urn in ({sb.ToString().TrimEnd(',')})";
-
-            var results = new List<EfficiencyMetricDataObject>();
-            try
-            {
-                var query = container.GetItemQueryIterator<EfficiencyMetricDataObject>(new QueryDefinition(queryString));
-                while (query.HasMoreResults)
-                {
-                    var response = await query.ReadNextAsync();
-
-                    results.AddRange(response.ToList());
-                }
-                if (results.Count < urns.Count)
-                {
-                    throw new Newtonsoft.Json.JsonSerializationException();
-                }
-                return results;
-            }
-            catch (Exception ex)
-            {
-                var errorMessage = $"EM collection could not be loaded! : {ex.Message} : URNs = {sb.ToString().TrimEnd(',')}";
-                base.LogException(ex, errorMessage);
-                throw new ApplicationException($"One or more documents could not be loaded from EM collection : URNs = {sb.ToString().TrimEnd(',')}");
-            }
-
+            var feedIterator = container.GetItemQueryIterator<EfficiencyMetricParentDataObject>(queryDefinition, null);
+            return (await feedIterator.ReadNextAsync()).First();            
         }
     }
 }
