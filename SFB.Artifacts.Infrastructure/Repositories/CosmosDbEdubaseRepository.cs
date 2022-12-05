34adf40b9765654fd34aab8806c3f499da148fe4
Jon Banner <jon@jonbanner.net>
2022-12-05 12:59:00 +0000

    Surface ofsted inspection date in getMultiple endpoint

--- SFB.Artifacts.Infrastructure/Repositories/CosmosDbEdubaseRepository.cs
+++ SFB.Artifacts.Infrastructure/Repositories/CosmosDbEdubaseRepository.cs
@@ -312,6 +312,7 @@ namespace SFB.Web.Infrastructure.Repositories
                 $"c['{EdubaseDataFieldNames.NO_PUPIL}'], " +
                 $"c['{EdubaseDataFieldNames.RELIGIOUS_CHARACTER}'], " +
                 $"c['{EdubaseDataFieldNames.OFSTED_RATING}'], " +
+                $"c['{EdubaseDataFieldNames.OFSTE_LAST_INSP}'], " +
                 $"c['{EdubaseDataFieldNames.LOCATION}'], "+
                 $"c['{EdubaseDataFieldNames.FINANCE_TYPE}'], c['{EdubaseDataFieldNames.IS_FEDERATION}'], c['{EdubaseDataFieldNames.IS_PART_OF_FEDERATION}'], " +
                 $"c['{EdubaseDataFieldNames.FEDERATION_UID}'], c['{EdubaseDataFieldNames.FEDERATION_NAME}'], c['{EdubaseDataFieldNames.FEDERATION_MEMBERS}'], " +
